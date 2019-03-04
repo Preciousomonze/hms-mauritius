ba2eda6bd811ba53c8c4402fb514cc777741531a
Omonzejele Precious <crispusomonze@gmail.com>
2019-03-04 21:33:06 +0100

    fixed check_avail and logout

--- hms/check_availability.php
+++ hms/check_availability.php
@@ -3,8 +3,8 @@ require_once("include/config.php");
 if(!empty($_POST["email"])) {
 	$email= $_POST["email"];
 	
-		$result =mysqli_query($con,"SELECT email FROM users WHERE email='$email'");
-		$count=mysqli_num_rows($result);
+		$result =$db_query->get("SELECT email FROM users WHERE email=?",[$email]);
+		$count=$db_query->get_row_count();
 if($count>0)
 {
 echo "<span style='color:red'> Email already exists .</span>";
--- hms/logout.php
+++ hms/logout.php
@@ -4,7 +4,12 @@ include('include/config.php');
 $_SESSION['login']=="";
 date_default_timezone_set('Asia/Kolkata');
 $ldate=date( 'd-m-Y h:i:s A', time () );
-mysqli_query($con,"UPDATE userlog  SET logout = '$ldate' WHERE uid = '".$_SESSION['id']."' ORDER BY id DESC LIMIT 1");
+
+$data_to_change = array('logout'=>'?');
+$data_to_bind = array($ldate,$_SESSION['id']);
+//user log table doesnt exist, so there'll be an error shaa, put the user log table
+$db_query->change("userlog",$data_to_change,"WHERE uid = ? ORDER BY id DESC LIMIT 1",$data_to_bind);
+//mysqli_query($con,"UPDATE userlog  SET logout = '$ldate' WHERE uid = '".$_SESSION['id']."' ORDER BY id DESC LIMIT 1");
 session_unset();
 //session_destroy();
 $_SESSION['errmsg']="You have successfully logout";
